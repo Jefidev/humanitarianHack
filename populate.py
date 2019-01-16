e5fe5fe659ba24092c221a32bc1e5a80b00155b9
Jérôme Fink <jerome.fink@student.unamur.be>
2019-01-16 11:28:04 +0100

    testing

--- populate.py
+++ populate.py
@@ -1,42 +1,42 @@
-import json
 import base64
-import os
-from random import randint,choice
 import datetime
+import json
+import os
+from random import choice, randint
 
 
 def random_list_user():
     path_image = "./resources/face/"
     images = os.listdir(path_image)
 
-    list_psw = ["psw","cpi","coco"]
-    list_sq = ["papa","dobby","TheBest"]
+    list_psw = ["psw", "cpi", "coco"]
+    list_sq = ["papa", "dobby", "TheBest"]
 
-    names = ["Genin","Fink","Georis"]
-    firstnames = ["Simon","Jerome","Francois"]
+    names = ["Genin", "Fink", "Georis"]
+    firstnames = ["Simon", "Jerome", "Francois"]
 
     users = []
 
-    for i in range(0,len(images)) :
+    for i in range(0, len(images)):
 
         if(images[i].split(".")[-1] == "jpg"):
             user = {
-                "identifications":{},
-                "parameters":{
-                    "general_info":{}
+                "identifications": {},
+                "parameters": {
+                    "general_info": {}
                 }
             }
 
             identification = {}
 
-
             sourceFile = path_image+images[i]
             print(sourceFile)
 
-            imageSource = open(sourceFile,'rb')
-            encoded_img_b64 = base64.b64decode(imageSource.read())
+            imageSource = open(sourceFile, 'rb')
+            encoded_img_b64 = base64.b64encode(imageSource.read())
+            base64_string = encoded_img_b64.decode("utf-8")
 
-            identification["image"] = encoded_img_b64
+            identification["image"] = base64_string
             identification["psw"] = list_psw[i]
             identification["sq"] = list_sq[i]
 
@@ -47,15 +47,11 @@ def random_list_user():
             general_info["name"] = choice(names)
             general_info["firstname"] = choice(firstnames)
 
-            general_info["birthdate"]  = datetime.date(randint(1980,2010), randint(1,12),randint(1,28))
+            general_info["birthdate"] = datetime.date(
+                randint(1980, 2010), randint(1, 12), randint(1, 28))
 
             user["parameters"]["general_info"] = general_info
 
             users.append(user)
 
     return users
-
-
-
-        
-
